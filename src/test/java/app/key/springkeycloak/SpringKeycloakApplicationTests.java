3d75da4ca830273a3ac879dac075ad38fcc5d43e
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-14 10:31:29 +0300

    Keycloak

---
+++ src/test/java/app/key/springkeycloak/SpringKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package app.key.springkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
